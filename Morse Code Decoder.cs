37f2b03df19ea0385bafada0d311262c0c294cf1
Alex75 <alessandro.piccione.75@gmail.com>
2017-01-24 12:26:40 +0000

    Add 2 Kata (Disemvowel and Morse Code Detector).

---
+++ Morse Code Decoder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    class MorseCodeDecoder
+    {
+        public static string Decode(string morseCode)
+        {
+            string[] morseWords = Regex.Split(morseCode, "   ");
+            StringBuilder finalText = new StringBuilder();
+            foreach (var morseWord in morseWords)
+            {
+                string[] morseLetters = morseWord.Split(' ');
+                if(finalText.Length == 0)
+                    finalText.Append(morseCode.Get())
+            }
+            return string.Join(" ", words);
+            
+
+            //MorseCode.Get()
+        }
+    }
+
+
+    class MorseCode
+    {
+        static string Get(string a) { return "a"; }
+    }
+
+
+    [TestFixture]
+    public class MorseCodeDecoderTests
+    {
+        [Test]
+        public void MorseCodeDecoderBasicTest_1()
+        {
+            try
+            {
+                string input = ".... . -.--   .--- ..- -.. .";
+                string expected = "HEY JUDE";
+
+                string actual = MorseCodeDecoder.Decode(input);
+
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("There seems to be an error somewhere in your code. Exception message reads as follows: " + ex.Message);
+            }
+        }
+    }
+}
