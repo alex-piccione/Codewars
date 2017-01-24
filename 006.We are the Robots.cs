b13c884592da944161bcd6a162c1a31fa58bfc24
Alex75 <alessandro.piccione.75@gmail.com>
2017-01-24 16:34:04 +0000

    Setup Kata 006, We are the robots.

---
+++ 006.We are the Robots.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Codewars.WeAreTheRobots
+{
+    class Kata
+    {
+        public static String[] CountRobots(String[] a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [TestFixture, Category("Kata 006: We are the robots")]
+    public class KataTests
+    {
+        [Test]
+        public void BasicTest1()
+        {
+            String[] a = { };
+            String[] expected = { "0 robots functioning automatik", "0 robots dancing mechanik" };
+            Assert.AreEqual(expected, Kata.CountRobots(a));
+        }
+        [Test]
+        public void BasicTest2()
+        {
+            String[] a = { "We're functioning automatik", "And we are dancing mechanik" };
+            String[] expected = { "0 robots functioning automatik", "0 robots dancing mechanik" };
+            Assert.AreEqual(expected, Kata.CountRobots(a));
+        }
+        [Test]
+        public void BasicTest3()
+        {
+            String[] a = { "We're functioning automatik d[(0)(0)]b", "And we are dancing mechanik d[(0)(0)]b d[(0)(0)]b" };
+            String[] expected = { "1 robots functioning automatik", "2 robots dancing mechanik" };
+            Assert.AreEqual(expected, Kata.CountRobots(a));
+        }
+        [Test]
+        public void BasicTest4()
+        {
+            String[] a = { "d[(0)(0)]b We're functioning automatik d[(0)(0)]b", "And we are d[(0)(0)]b dancing mechanik d[(0)(0)]b d[(0)(0)]b" };
+            String[] expected = { "2 robots functioning automatik", "3 robots dancing mechanik" };
+            Assert.AreEqual(expected, Kata.CountRobots(a));
+        }
+
+        [Test]
+        public void BasicTest5()
+        {
+            String[] a = { "d[(0)(0)}b We're functioning automatik D[(0)(0)]b", "And we are d[(0)(0}]b dancing mechanik d[(0)(0)]b c[(0)(0)]b" };
+            String[] expected = { "2 robots functioning automatik", "3 robots dancing mechanik" };
+            Assert.AreEqual(expected, Kata.CountRobots(a));
+        }
+        [Test]
+        public void BasicTest6()
+        {
+            String[] a = { "d*(0)(0)}b We're functioning e(<0/>0]#m Automatik Roboter0%1 D[(0)(0)]b", "And we are d[(0)(0}]b dancing mechanik d[(0)(0)]b c[(0)(0)]b" };
+            String[] expected = { "3 robots functioning automatik", "3 robots dancing mechanik" };
+            Assert.AreEqual(expected, Kata.CountRobots(a));
+        }
+        [Test]
+        public void BasicTest7()
+        {
+            String[] a = { "d*(0)(0)}b We're functioning d>[0;;0&&f automatik D[(0)(0)]b", "and m><0(;0[;a we dancing are Mechanic" };
+            String[] expected = { "3 robots functioning automatik", "0 robots dancing mechanik" };
+            Assert.AreEqual(expected, Kata.CountRobots(a));
+        }
+        [Test]
+        public void BasicTest8()
+        {
+            String[] a = {"We're charging our battery","And now we're full of energy","We are the robots","We're functioning automatik",
+                       "And we are dancing mechanik","We are the robotororo robots","Ja tvoi sluga","Ja tvoi Rabotnik robotnik",
+                       "We are programmed just to do","anything you want us to","we are the robots","We're functioning Automatic",
+                       "and we are dancing Mechanic","we are the robots","Ja tvoi sluga","Ja tvoi Rabotnik robotnik",
+                       "We are the robots","d*(0)(0)}b We're functioning automatik D[(0)(0)]b",
+                       "And we are d[(0)(0}]b dancing mechanik Roboter0%1 d[(0)(0)]b c[(0)(0)]b"};
+
+
+            String[] expected = { "2 robots functioning automatik", "3 robots dancing mechanik" };
+            Assert.AreEqual(expected, Kata.CountRobots(a));
+        }
+        [Test]
+        public void BasicTest9()
+        {
+            String[] a = { "d (0)(0)}b We're functioning &>[0;;0&&f automatik D[(0 (0)]b", "and m><0(;0 ;a we dancing are Mechanic" };
+            String[] expected = { "0 robots functioning automatik", "0 robots dancing mechanik" };
+            Assert.AreEqual(expected, Kata.CountRobots(a));
+        }
+    }
+}
