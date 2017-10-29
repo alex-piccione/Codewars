bb65d1440693ad202ad15a6e169202c42323ee0f
Alex75 <alessandro.piccione.75@gmail.com>
2017-10-29 11:37:25 +0000

    JavaScript. Add removeBMW kata.

---
+++ JavaScript/Kyu 7/removeBMW.js
@@ -0,0 +1,31 @@
+function removeBMW(str){
+    
+    if(typeof(str) !== "string" || typeof(str) === "number")
+        throw Error("This program only works for text.");
+
+    return str.replace(/[bmw]+/ig, "");
+}
+    
+  
+
+Test = {
+    assertEquals: function( result, expected) {
+        if(result != expected) console.error(". Expected: " + expected + ", result: " + result);
+    },
+    expectError: function(message, action) {
+        try{
+            action()
+        }
+        catch (err){
+            return;
+        }
+
+        console.error(message);
+    }
+}
+
+
+//Test.assertEquals(removeBMW("bmwvolvoBMW"), "volvo");
+//Test.assertEquals(removeBMW("bbmmww"), "");
+Test.expectError("pass number 2", function() {removeBMW(2)});
+//Test.expectError("It should throw an error when an array is sent as an argument", function(){ removeBMW([]) });
