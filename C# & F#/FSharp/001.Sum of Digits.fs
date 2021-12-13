0d6db8345fe05b4f6d60959b0d14778ae92a1c38
Alex <alessandro.piccione.75@gmail.com>
2021-12-13 20:49:22 +0000

    solve simple case

--- C# & F#/FSharp/001.Sum of Digits.fs
+++ C# & F#/FSharp/001.Sum of Digits.fs
@@ -1,17 +1,22 @@
 ﻿module FSharp.DigitalRoot
 
+open System
 open NUnit.Framework
 open FsUnit
 
+let charToInt = System.Globalization.CharUnicodeInfo.GetDigitValue 
 
-let rec digitalRoot n = 
 
+let rec digitalRoot (n:int) =
+    let digits = List.ofArray(n.ToString().ToCharArray())
 
-    failwith "Not implemented yet."
+    let count sum a =
+        sum + (charToInt a)
 
+    digits |> List.fold count 0
 
 
 
 [<Test>]
-let test() =
-    digitalRoot 16 should be 7
+let ``Digital Root``() =
+    digitalRoot 16 |> should equal 7
