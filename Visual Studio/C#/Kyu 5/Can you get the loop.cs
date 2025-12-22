fcd22eb2f257302b59955d4a2d4fd017439a0fa0
Alex <alessandro.piccione.75@gmail.com>
2025-12-22 12:59:52 +0100

    Add new C# Kata

---
+++ Visual Studio/C#/Kyu 5/Can you get the loop.cs
@@ -0,0 +1,91 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+
+public class LoopDetector
+{
+    public class Node
+    {
+        public Node? next;
+    }
+
+    // Helper method to create a chain with a loop
+    public static Node createChain(int nonLoopSize, int loopSize)
+    {
+        Node head = new();
+        Node current = head;
+        Node? loopStart = null;
+
+        for (int i = 0; i < (nonLoopSize+loopSize-1); i++)
+        {
+            Node next = new();
+            current.next = next;
+
+            if (i == nonLoopSize)
+                loopStart = current;
+
+            // move on
+            current = next;
+        }
+
+        // close the loop
+        current.next = loopStart;
+
+        return head;
+    }
+}
+
+public class Kata
+{
+    public static int getLoopSize(LoopDetector.Node startNode)
+    {
+        System.Collections.Generic.List<LoopDetector.Node> nodes = [];
+
+        while (true)
+        {
+            var nextNode = startNode.next ?? throw new System.Exception("not a chain");
+            if (nodes.Contains(nextNode))
+                return nodes.Count - nodes.IndexOf(nextNode);
+            else
+            {
+                nodes.Add(nextNode);
+                startNode = nextNode;
+            }
+        }
+    }
+}
+
+
+
+[TestFixture]
+public class Tests
+{
+    [Test, Order(1)]
+    public void FourNodesWithLoopSize3()
+    {
+        var n1 = new LoopDetector.Node();
+        var n2 = new LoopDetector.Node();
+        var n3 = new LoopDetector.Node();
+        var n4 = new LoopDetector.Node();
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n2;
+        Assert.That(Kata.getLoopSize(n1), Is.EqualTo(3));
+    }
+
+    [Test, Order(2)]
+    public void RandomChainNodesWithLoopSize30()
+    {
+        var n1 = LoopDetector.createChain(3, 30);
+        Assert.That(Kata.getLoopSize(n1), Is.EqualTo(30));
+    }
+
+    [Test, Order(3)]
+    public void RandomLongChainNodesWithLoopSize1087()
+    {
+        var n1 = LoopDetector.createChain(3904, 1087);
+        Assert.That(Kata.getLoopSize(n1), Is.EqualTo(1087));
+    }
+}
---
+++ Visual Studio/C#/Kyu 5/Can you get the loop.md
@@ -0,0 +1,19 @@
+﻿# Can you get the loop ?
+
+https://www.codewars.com/kata/52a89c2ea8ddc5547a000863/csharp
+
+## Description:  
+You are given a node that is the beginning of a linked list.  
+This list contains a dangling piece and a loop. Your objective is to determine the length of the loop.  
+  
+For example in the following picture the size of the dangling piece is 3 and the loop size is 12:  
+![../images/Can_you_get_the_loop_1]
+
+  
+Use the _next_ method to get the following node.  
+``node.next``  
+
+## Notes:
+
+- do NOT mutate the nodes!
+- in some cases there can be just a loop, with no dangling piece.
