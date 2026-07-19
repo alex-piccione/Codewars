1fa16a1fcd421d259383b12613dc6225838bc5bd
Alex <alessandro.piccione.75@gmail.com>
2026-07-19 17:08:40 +0200

    add C# kyu 5 kata

---
+++ C# & F#/C#/Kyu 5/Count IP Addresses.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Codewars.Kyu_5
+{
+    public class CountIPAddresses
+    {
+        public static long IpsBetween(string start, string end)
+        {
+            var start_subs = start.Split('.').Select(x => int.Parse(x)).ToArray();
+            var end_subs = end.Split('.').Select(x => int.Parse(x)).ToArray();
+
+            return
+                (end_subs[3] - start_subs[3]) +
+                (end_subs[2] - start_subs[2]) * 256L +
+                (end_subs[1] - start_subs[1]) * 256L * 256L +
+                (end_subs[0] - start_subs[0]) * 256L * 256L * 256L
+                ;
+        }
+    }
+
+    public class CountIPAddressesTest
+    {
+        [Test]
+        public void SmapleTest()
+        {
+            Assert.That(CountIPAddresses.IpsBetween("10.0.0.0", "10.0.0.50"), Is.EqualTo(50));
+            Assert.That(CountIPAddresses.IpsBetween("20.0.0.10", "20.0.1.0"), Is.EqualTo(246));
+            Assert.That(CountIPAddresses.IpsBetween("0.0.0.0", "255.255.255.255"), Is.EqualTo((1L << 32) - 1L));
+        }
+    }
+}
--- C# & F#/C#/README.md
+++ C# & F#/C#/README.md
@@ -8,3 +8,5 @@
 ## Kyu 5
 
 - Mean Square Error: https://www.codewars.com/kata/51edd51599a189fe7f000015/train/csharp
+- Geometric Series of Events: https://www.codewars.com/kata/68379963cbaf46ff456c2899
+- Count IP Addresses: https://www.codewars.com/kata/526989a41034285187000de4/csharp
